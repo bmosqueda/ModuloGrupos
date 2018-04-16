f8bbce75d833d4ece5b594b24dc8e8f61b696ba6
Brandon Alejandro Mosqueda González <brandonMG_1698@hotmail.com>
2018-04-16 08:37:01 -0500

    Diseño de la base de datos agregado a Proyecto servicio info

---
+++ Proyecto servicio info/Database.c
@@ -0,0 +1,104 @@
+Usuario
+{
+	+ id: int
+	+ numeroCuenta: int
+	+ nombre: string
+	+ apellidoPaterno: string
+	+ apellidoMaterno: string
+	+ correo: string
+	+ idPlantel: int 
+}
+
+RolUsuarios
+{
+	+ id: int 
+	+ idUsuario: int 
+	+ idRol: int 
+}
+
+//Roles{master, administrador, coordinador, profesor}
+Rol
+{
+	+ id: int 
+	+ tipo: string
+}
+
+//Incluye facultades y bachilleratos
+Plantel
+{
+	+ id: int 
+	+ nombre: string
+}
+
+CarreraArea //Para los bachilleratos se manejará como área
+{
+	+ id: int
+	+ plan: string	//Es como una mátricula única
+	+ nombre: string
+	+ idPlantel: int
+}
+
+Alumno //El alumno es un usuario o qué es el autoregristro
+{
+	+ int: id
+	+ numeroCuenta: int 
+	+ apellido paterno: string
+	+ apellido materno: string
+	+ correo: string
+	+ nombre: string
+	+ idTipoAlumno: int 
+}
+
+/*//{normal, invitado, irregular, intercambio }
+Tipo de alumno
+{
+	+ id: int 
+	+ string: tipo
+}
+Profesor
+{
+	+ id: int 
+	+ usuario: int 
+	+ idMateria: int
+}*/
+
+Materia//Como los profesores pueden tener muchos grupos y los grupos muchos profesores se crea esta tabla intermediaria
+{
+	+ id: int 
+	+ idGrupo: int 
+	//+ idUsuario: int //Profesor
+	+ string: nombre
+}
+
+MateriasProfesor
+{
+	+ id: int 
+	+ idProfesor: int 
+	+ idMateria: int 
+}
+
+Grupo
+{
+	+ int: id
+	+ grado: int
+	+ grupo: char
+	+ generación: Date	//o fecha de inicio de la generación
+	+ periodos: int	
+	+ idCarrera: int
+	+ esOficial: bool 	//Porque se podrá dar de alta grupos no oficiales
+}
+
+GruposAlumno
+{
+	+ id: int 
+	+ idAlumno: int 
+	+ idGrupo: int 
+}
+
+LogBajas //Cómo se gestiona esto, en una tabla o en arvhivos de texto
+{
+	+ int: id
+	+ usuario: int
+	+ alumno: int
+	+ fechaBaja: Date
+}
